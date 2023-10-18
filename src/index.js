2d37c4dd473f09ac31b7aa29cc4f9aa0b0236ced
MiddleDcoder <lorenzjhun.abidinasna@gmail.com>
2023-10-18 11:45:50 +0800

    Dynamic Content

--- src/index.js
+++ src/index.js
@@ -1,14 +1,20 @@
 import React from 'react'; //React -> is a variable for the package 'react' (manage elements)
 import ReactDOM from 'react-dom/client';  //ReactDOM -> render the elements into the documents
 
-// .createElement() -> method for creating elements 3 parameter: 1.The Element 2.The Attribute 3.Inner Content
-const div = React.createElement('div', null, [
-  // creating children createElement by making the 3rd parameter to an array in then re-createElement
-  React.createElement('h1', null, 'Hi'),
-  React.createElement('p', null, 'Hello'),
-  React.createElement('p', null, 'Hey'),
-]);
+// converter the variable to a function to avoid memory leak
+function Page() {
+  // .createElement() -> method for creating elements 3 parameter: 1.The Element 2.The Attribute 3.Inner Content
+  return React.createElement('div', null, [
+    React.createElement('h1', null, `Hi ${Date().toLocaleString()}`), //added date to current local time
+    React.createElement('p', null, 'Hello'),
+    React.createElement('p', null, 'Hey'),
+  ]);
+}
 const rootEl = document.querySelector('#root'); //.querySelector() -> to select the element root
 const root = ReactDOM.createRoot(rootEl); //this function accepts element from the document
 
-root.render(div); //display an element on the page by .render()
+//special function for executing another function on an interval.
+setInterval(function () {
+  root.render(Page());  //display an element on the page by .render()
+}, 1000);
+
