6b677512c59fadf2bf6aa61f794d80d77c21b2c0
MiddleDcoder <lorenzjhun.abidinasna@gmail.com>
2023-10-21 18:55:21 +0800

    Using useEffect

--- src/Counter.js
+++ src/Counter.js
@@ -3,6 +3,18 @@ import React from 'react';
 export default function () {
     const [count, setCount] = React.useState(0);
 
+    //check if the count item exists in the storage. If it does, we should set the initial value of the count state to the count from the local storage
+    React.useEffect(() => {
+        if(localStorage.getItem('count')) {
+            setCount(parseInt(localStorage.getItem('count')));
+        }
+    }, []);
+
+    //Track the count variable that when updated we will run a function to store in storage
+    React.useEffect(() => {
+        localStorage.setItem('count', count);
+    }, [count]);
+
     // function to handle the click callback behavior we want
     function handleClick(event) { 
         event.preventDefault(); //we pass in the object event and access the preventDefault to avoid changing the url
